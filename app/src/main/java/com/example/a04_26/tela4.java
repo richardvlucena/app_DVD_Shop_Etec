a9dbe058aa42a352cd8eaa2e57c7fdba8b05e3b7
richardvlucena <richard.v.lucena@protonmail.com>
2022-05-22 14:00:31 -0300

    add 3 doc on screens 3,4 and 5

--- app/src/main/java/com/example/a04_26/tela4.java
+++ app/src/main/java/com/example/a04_26/tela4.java
@@ -3,6 +3,9 @@ package com.example.a04_26;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class tela4 extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class tela4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela4);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mango, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.cadastro:
+                cadastro();
+                return true;
+            case R.id.sair:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void cadastro() {
     }
 }
