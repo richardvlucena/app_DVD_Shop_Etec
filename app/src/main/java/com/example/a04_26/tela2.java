7ca837ea7947c990d7631a88270b93a5bd2f3663
richardvlucena <richard.v.lucena@protonmail.com>
2022-05-22 13:57:50 -0300

    hunted  btn and  screen

--- app/src/main/java/com/example/a04_26/tela2.java
+++ app/src/main/java/com/example/a04_26/tela2.java
@@ -3,6 +3,9 @@ package com.example.a04_26;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class tela2 extends AppCompatActivity {
 
@@ -12,5 +15,27 @@ public class tela2 extends AppCompatActivity {
         setContentView(R.layout.activity_tela2);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mango, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.cadastro:
+                cadastro();
+                return true;
+            case R.id.sair:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void cadastro() {
+    }
 }
